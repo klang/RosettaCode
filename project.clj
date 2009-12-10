647bf643d7a28172319bc37dc81955e7fac5d04c
Karsten Lang <karstenlang@gmail.com>
2009-12-10 22:58:25 +0100

    project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject RosettaCode "0.1"
+  :description "Translation programming tasks from Rosetta Code. http://rosettacode.org"
+  :url "http://github.com/klang/RosettaCode"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+		 ]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]]
+  :main dispatch)
+
