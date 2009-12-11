33323e5e8359fd16fd86e070867d13bb382f4097
Karsten Lang <karstenlang@gmail.com>
2009-12-11 08:11:37 +0100

    removing a bit of trash and renaming function

--- src/sierpinski-carpet.clj
+++ src/sierpinski-carpet.clj
@@ -31,19 +31,15 @@
 
 ;; development process kept for confusion 
 
-(defn sierpinski-carpet [order] nil)
+(defn in-carpet? [x y] true)
 
-(defn in-carpet? [x y]
-  true
-)
-
-(defn carpet [order]
+(defn sierpinski-carpet [order]
   (let [size (expt 3 order)]
     (for [i (range size)] 
       (for [j (range size)] 
 	(if (in-carpet? i j) "*" " ")))))
 
-(defn carpet [n]
+(defn sierpinski-carpet [n]
   (for [i (range (expt 3 n))] 
     (for [j (range (expt 3 n))] 
       (if (in-carpet? i j) "*" " "))))
@@ -51,47 +47,35 @@
 (defn jline [i n]
   (println (apply str (vec (map #(if (in-carpet? i %) "*" " ") (range (expt 3 n)))))))
 
-(defn carpet [n]
+(defn sierpinski-carpet [n]
   (for [i (range (expt 3 n))] 
     (jline i n)))
 
 (defn iline [n] 
   (println (apply str (vec (map #(jline % n) (range (expt 3 n)))))))
 
-(defn carpet [n]
+(defn sierpinski-carpet [n]
   (iline n))
 
-(defn carpet [n]
-  (println (apply str (vec (map #(jline % n) (range (expt 3 n))))))
-  )
-
+(defn sierpinski-carpet [n]
+  (println (apply str (vec (map #(jline % n) (range (expt 3 n)))))))
 
 (defn in-carpet? [x y]
   (loop [x x
 	 y y]
     (cond (or (zero? x) (zero? y) true)
 	  (and (= 1 (mod x 3)) (= 1 (mod y 3)))
-	  :else nil)
-    )
-)
+	  :else nil)))
 
 (defn a [x y] (or (zero? x) (zero? y)))
 (defn b [x y] (and (= 1 (mod x 3)) (= 1 (mod y 3))))
-
-(defn mytest [x y]
-  (cond (a x y) true
-	(b x y) false
-	))
 
 (defn in-carpet? [x y]
   (loop [x x
 	 y y]
     (cond (a x y) true
 	  (b x y) false
-	  :else (recur (quot x 3) (quot y 3))
-	)
-    )
-)
+	  :else (recur (quot x 3) (quot y 3)))))
 
 ;; final (based on scheme version)
 ;; this version is submitted to RosettaCode.org
@@ -121,13 +105,14 @@
 
 (use '[clojure.contrib.str-utils :only (str-join)])
 
-(defn carpet [n]
+(defn sierpinski-carpet [n]
   (apply str 
 	 (interpose \newline 
 		    (for [i (range (expt 3 n))] 
 		      (apply str (vec (for [j (range (expt 3 n))] 
 					(if (in-carpet? i j) "*" " "))))))))
 
+;; final (based on scheme)
 ;; this version is submitted to RosettaCode.org (with in-carpet? above)
 ;; http://rosettacode.org/wiki/Sierpinski_carpet#Clojure
 (defn carpet [n]
@@ -137,6 +122,7 @@
 		      (apply str (for [j (range (expt 3 n))] 
 					(if (in-carpet? i j) "*" " ")))))))
 
+;; alternative, using an external library
 (use '[clojure.contrib.str-utils :only (str-join)])
 ;; "(apply str (interpose" is better written as "str-join"
 (defn carpet [n]
@@ -166,5 +152,7 @@
 		      (apply str (for [j (range (expt 3 n))] 
 					(if (in-carpet? i j) "*" " ")))))))
 
+(comment
 (print sierpinski-carpet 3)
+)
 ;;-----------------------------------------------------------------------------
