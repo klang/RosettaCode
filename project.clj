927d41874bac7d9983df1acb109ad6c68e3ee9a5
Karsten Lang <klang@endjinn.(none)>
2010-02-18 15:30:30 +0100

    getting up to speed

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject RosettaCode "0.1"
   :description "Translation programming tasks from Rosetta Code. http://rosettacode.org"
   :url "http://github.com/klang/RosettaCode"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
 		 ]
   :dev-dependencies [[org.clojure/swank-clojure "1.0"]]
   :main dispatch)
