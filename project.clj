575e35070a042ed2d159c3ceef73aa6ec705fb1f
Karsten Lang <karstenlang@gmail.com>
2011-04-01 19:06:43 +0200

    swank update

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject RosettaCode "0.1"
   :description "Translation programming tasks from Rosetta Code. http://rosettacode.org"
   :url "http://github.com/klang/RosettaCode"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  ;;:jar-behavior :symlink
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
 		 ]
-  :dev-dependencies [[org.clojure/swank-clojure "1.0"]]
-  :main dispatch)
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  ;:main dispatch
+  :jvm-opts ["-Xmx512M"])
 
